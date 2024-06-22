2b110eecc5b1f0d769f28ac70acde4881225edf2
PHChenGit <chenpohsun1@gmail.com>
2024-06-22 19:08:11 +0800

    feat: Remove nerdtree, change to use neotree

--- lua/plugins/nerdtree.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "preservim/nerdtree",
-    keys = {
-        {"<C-n>", ":NERDTree<CR>", {desc = "Open NerdTree"}},
-        {"<C-t>", ":NERDTreeToggle<CR>", {desc = "Toogle NerdTree"}},
-        {"<C-f>", ":NERDTreeFind<CR>", {desc = "Find NerdTree"}}
-    },
-}
